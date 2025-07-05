be3aa67f78648e2412ab5cb04a33878566231c16
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-07-05 17:21:40 +0000

    feat: adding contact model and connecting to db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 
 const app = express()
 
@@ -7,7 +11,19 @@ app.use(express.json())
 app.use(cors())
 
 
+//  POST
 
+
+// GET
+
+
+
+
+// DELETE
+
+
+
+// PUT
 
 
 

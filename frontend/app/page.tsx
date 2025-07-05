94ae3bcf260d13f25dacb204a71fe8f77823ef8b
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-07-05 19:12:03 +0000

    feat: adding some shadcn ui components and adding axios lib

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,18 @@
 // import Image from "next/image";
 
+import { Button } from "@/components/ui/button"
+
+
 const Home = () => {
 
   return (
     <>
-    
+      Olá, está funcionando
+      Botão shadcn:
+      <br />
+      <Button>
+        Botão
+      </Button>
     </>
   )
 }
